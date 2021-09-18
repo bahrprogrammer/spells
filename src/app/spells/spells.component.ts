f9d3262114f9c22de32601c58bc50ef25dbc37ae
JeffreyBahr <bahr.programmer@gmail.com>
2021-09-18 13:50:24 -0600

    Prettified the all spells page

--- src/app/spells/spells.component.ts
+++ src/app/spells/spells.component.ts
@@ -8,28 +8,107 @@ import { ISpell } from '../models/ISpells';
   styleUrls: ['./spells.component.scss']
 })
 export class SpellsComponent implements OnInit {
-  selectedSpell: ISpell;
+  storage = window.localStorage;
+  storageKey = 'spell-filter';
+  
   spells: ISpell[];
+  displayedSpells: ISpell[];
 
+  showOnlyConcentration = false;
+  showOnlyRitual = false;
 
   constructor(private spellService: SpellsService) { }
 
   ngOnInit(): void {
+    this.getLocalStorage();
     this.getSpells();
   }
 
   getSpells(): void {
     this.spellService.getSpells().subscribe({
-      next: (data) => (this.spells = data)
+      next: (data) => {
+        (this.spells = data);
+        this.filterChange();
+      }
     });
+  }  
+
+  addToLocalStorage() {
+    const localStorageAvailable = this.storageAvailable('localStorage');
+
+    if (localStorageAvailable) {
+      const filters: any = {
+        showOnlyConcentration: this.showOnlyConcentration,
+        showOnlyRitual: this.showOnlyRitual
+      };
+
+      this.storage.spellFilters = JSON.stringify(filters);
+    }
   }
 
-  onSubmit(): void {
-    const index = this.spells.indexOf(this.selectedSpell);
-    this.spells.splice(index, 1, this.selectedSpell);
+  getLocalStorage() {
+    const localStorageAvailable = this.storageAvailable('localStorage');
+
+    if (localStorageAvailable) {
+      const cache = this.storage.spellFilters;
+
+      if (cache) {
+        const filters: any = JSON.parse(cache);
+        this.showOnlyConcentration = filters.showOnlyConcentration;
+        this.showOnlyRitual = filters.showOnlyRitual;
+      }
+    }
   }
 
-  select(spell: ISpell): void {
-    this.selectedSpell = spell;
+  storageAvailable(type: any): any {
+    let storage;
+
+    try {
+      storage = window[type];
+      const x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return e instanceof DOMException && (
+        e.code === 22 ||
+        e.code === 1014 ||
+        e.name === 'QuotaExceededError' ||
+        e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+        (storage && storage.length !== 0);
+    }
+
+    return true;
   }
+
+  filterChange(): void {
+    this.displayedSpells = [];
+
+    if (this.showOnlyConcentration && this.showOnlyRitual) {
+      this.spells.forEach(s => {
+        if (s.concentration && s.ritual) {
+          this.displayedSpells.push(s);
+        }
+      });
+    } else if (this.showOnlyConcentration) {
+      this.spells.forEach(s => {
+        if (s.concentration) {
+          this.displayedSpells.push(s);
+        }
+      });
+    } else if (this.showOnlyRitual) {
+      this.spells.forEach(s => {
+        if (s.ritual) {
+          this.displayedSpells.push(s);
+        }
+      });
+    } else {
+      this.displayedSpells = [ ...this.spells ];
+    }
+
+    this.addToLocalStorage();
+  }
+
+
+
 }
